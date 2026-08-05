00aeeddaad0acf9dcfa74e5b840086fdfd061778
Mihai Ungureanu <92330332+mihaipxm@users.noreply.github.com>
2026-08-05 22:38:45 +0300

    chore: move to theme SDK 0.1.0-alpha.12 (#17)

--- theme.config.ts
+++ theme.config.ts
@@ -5,7 +5,7 @@ export default defineTheme({
   manifest: {
     id: "bucharest",
     name: "Bucharest",
-    version: "0.2.9",
+    version: "0.2.10",
     routes: [
       { id: "home", pattern: "/", context: "home" },
       { id: "content", pattern: "/[...path]", context: "content" },
