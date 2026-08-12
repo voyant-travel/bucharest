aa92b85e8c4da896e4e400a91d3864020320bdf4
Mihai Ungureanu <92330332+mihaipxm@users.noreply.github.com>
2026-08-12 08:32:19 +0300

    chore: release 0.9.5 (#43)

--- theme.config.ts
+++ theme.config.ts
@@ -5,7 +5,7 @@ export default defineTheme({
   manifest: {
     id: "bucharest",
     name: "Bucharest",
-    version: "0.9.4",
+    version: "0.9.5",
     routes: [
       { id: "home", pattern: "/", context: "home" },
       { id: "content", pattern: "/pages/[...path]", context: "content" },
