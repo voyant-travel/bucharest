fe593255cb1b618bb2d8b6b730e393bd7e45ee76
Mihai Ungureanu <mihai@pxm.digital>
2026-08-12 09:17:02 +0300

    chore: release 0.9.6

--- theme.config.ts
+++ theme.config.ts
@@ -5,7 +5,7 @@ export default defineTheme({
   manifest: {
     id: "bucharest",
     name: "Bucharest",
-    version: "0.9.5",
+    version: "0.9.6",
     routes: [
       { id: "home", pattern: "/", context: "home" },
       { id: "content", pattern: "/pages/[...path]", context: "content" },
